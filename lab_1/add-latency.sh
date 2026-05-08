a6723b00180bede3fee7d07e8512dfac2087e41f
Nicolas Michel <Nicolas@vpackets.net>
2026-05-08 16:38:17 -0500

    lab fix - script

--- lab_1/add-latency.sh
+++ lab_1/add-latency.sh
@@ -1,33 +1,16 @@
 #!/bin/bash
-
-sudo ip netns exec clab-cleu26-xrd01 tc qdisc add dev Gi0-0-0-1 root netem delay 10000
-sudo ip netns exec clab-cleu26-xrd01 tc qdisc add dev Gi0-0-0-2 root netem delay 5000
-
-sudo ip netns exec clab-cleu26-xrd02 tc qdisc add dev Gi0-0-0-1 root netem delay 30000
-sudo ip netns exec clab-cleu26-xrd02 tc qdisc add dev Gi0-0-0-2 root netem delay 20000
-
-sudo ip netns exec clab-cleu26-xrd03 tc qdisc add dev Gi0-0-0-1 root netem delay 40000
-
-sudo ip netns exec clab-cleu26-xrd04 tc qdisc add dev Gi0-0-0-1 root netem delay 30000
-sudo ip netns exec clab-cleu26-xrd04 tc qdisc add dev Gi0-0-0-2 root netem delay 30000
-
-sudo ip netns exec clab-cleu26-xrd05 tc qdisc add dev Gi0-0-0-2 root netem delay 5000
-
-sudo ip netns exec clab-cleu26-xrd06 tc qdisc add dev Gi0-0-0-0 root netem delay 30000
-
+sudo ip netns exec xrd01 tc qdisc add dev Gi0-0-0-1 root netem delay 10000
+sudo ip netns exec xrd01 tc qdisc add dev Gi0-0-0-2 root netem delay 5000
+sudo ip netns exec xrd02 tc qdisc add dev Gi0-0-0-1 root netem delay 30000
+sudo ip netns exec xrd02 tc qdisc add dev Gi0-0-0-2 root netem delay 20000
+sudo ip netns exec xrd03 tc qdisc add dev Gi0-0-0-1 root netem delay 40000
+sudo ip netns exec xrd04 tc qdisc add dev Gi0-0-0-1 root netem delay 30000
+sudo ip netns exec xrd04 tc qdisc add dev Gi0-0-0-2 root netem delay 30000
+sudo ip netns exec xrd05 tc qdisc add dev Gi0-0-0-2 root netem delay 5000
+sudo ip netns exec xrd06 tc qdisc add dev Gi0-0-0-0 root netem delay 30000
 echo "Latencies added. The following output applies in both directions, Ex: xrd01 -> xrd02 and xrd02 -> xrd01"
-
-echo "xrd01 link latency: "
-sudo ip netns exec clab-cleu26-xrd01 tc qdisc list | grep delay
-echo "xrd02 link latency: "
-sudo ip netns exec clab-cleu26-xrd02 tc qdisc list | grep delay
-echo "xrd03 link latency: "
-sudo ip netns exec clab-cleu26-xrd03 tc qdisc list | grep delay
-echo "xrd04 link latency: "
-sudo ip netns exec clab-cleu26-xrd04 tc qdisc list | grep delay
-echo "xrd05 link latency: "
-sudo ip netns exec clab-cleu26-xrd05 tc qdisc list | grep delay
-echo "xrd06 link latency: "
-sudo ip netns exec clab-cleu26-xrd06 tc qdisc list | grep delay
-
+for n in xrd01 xrd02 xrd03 xrd04 xrd05 xrd06; do
+    echo "$n link latency: "
+    sudo ip netns exec $n tc qdisc list | grep delay
+done
 
